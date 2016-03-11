fb8456e6785490152137627dd97aeebeb92779ce
liuhuiguang <liuhuiguang@gtan.com>
2016-03-11 18:56:43 +0800

    service activity relation test

--- app/src/main/java/example/com/servicetest/FrontActivity.java
+++ app/src/main/java/example/com/servicetest/FrontActivity.java
@@ -3,6 +3,7 @@ package example.com.servicetest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class FrontActivity extends AppCompatActivity {
 
@@ -15,5 +16,7 @@ public class FrontActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this,NotifactionService.class);
         startService(intent);
+        Log.i("aaa","activity:  pid: " +android.os.Process.myPid() + "\t thread: " + Thread.currentThread().getId() + "\t uid: " + android.os.Process.myUid() + "\t tid: " + android.os.Process.myTid() + "\t ppid: "
+                );
     }
 }
--- app/src/main/java/example/com/servicetest/NotifactionService.java
+++ app/src/main/java/example/com/servicetest/NotifactionService.java
@@ -4,12 +4,17 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class NotifactionService extends Service {
     public NotifactionService() {
@@ -20,6 +25,7 @@ public class NotifactionService extends Service {
     public void onCreate() {
         super.onCreate();
         test();
+        Log.i("aaa","notification:  pid: " +android.os.Process.myPid() + "\t thread: " + Thread.currentThread().getId() + "\t uid: " + android.os.Process.myUid() + "\t tid: " + android.os.Process.myTid() );
     }
 
     @Override
@@ -29,35 +35,82 @@ public class NotifactionService extends Service {
     }
 
     void test() {
+        //简版
         RemoteViews remoteViews = new RemoteViews(getPackageName(),
                 R.layout.notification);
+
+        View mRoot = LayoutInflater.from(this).inflate(R.layout.notification,null);
+        final Context context = this;
+        mRoot.findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"abcde",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContent(remoteViews);
+//                        .setContent(remoteViews)
+                        .setNumber(1)
+                        .setContentInfo("1s")
+//                        .setAutoCancel(true)
+                ;
 
 
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, FrontActivity.class);
-
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(FrontActivity.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(
-                        0, PendingIntent.FLAG_UPDATE_CURRENT);
+        Intent resultIntent = new Intent(this,NotificationResultActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this,111,resultIntent,PendingIntent.FLAG_NO_CREATE);
         mBuilder.setContentIntent(resultPendingIntent);
-        mBuilder.addAction(R.id.button1,"aa",resultPendingIntent);
 
+        int mNotificationId = 001;
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify("abc",mNotificationId, mBuilder.build());
 
+        //Regular Activity PendingIntent
+        /*int id = 1;
+        Intent resultIntent = new Intent(this, NotificationResultActivity.class);
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+        taskStackBuilder.addParentStack(NotificationResultActivity.class);
+        taskStackBuilder.addNextIntent(resultIntent);
+
+        PendingIntent resultPendingIntent = taskStackBuilder.getPendingIntent(111, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)        .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("aa").setContentText("bb").setAutoCancel(true);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify(id, mBuilder.build());*/
+
+        /*
+        Special Activity PendingIntent
+        // Instantiate a Builder object.
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this).setContentTitle("cc").setContentText("dd");
+        // Creates an Intent for the Activity
+        Intent notifyIntent =
+                new Intent(new ComponentName(this, NotificationResultActivity.class).toString());
+        // Sets the Activity to start in a new, empty task
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        // Creates the PendingIntent
+        PendingIntent notifyIntent1 =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        notifyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        // Puts the PendingIntent into the notification builder
+        builder.setContentIntent(notifyIntent1);
+        // Notifications are issued by sending them to the
+        // NotificationManager system service.
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        // mId allows you to update the notification later on.
-        mNotificationManager.notify(0, mBuilder.build());
+        // Builds an anonymous Notification object from the builder, and
+        // passes it to the NotificationManager
+        mNotificationManager.notify(0, builder.build());*/
+
+
+
     }
 }
