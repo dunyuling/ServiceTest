0f835d9783b71e033681a1a9c8762d76f428c978
liuhuiguang <liuhuiguang@gtan.com>
2016-03-15 19:21:26 +0800

    为另外程序提供服务

--- app/src/main/java/example/com/servicetest/MyRemoteService.java
+++ app/src/main/java/example/com/servicetest/MyRemoteService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.util.Log;
 
 public class MyRemoteService extends Service {
     public MyRemoteService() {
@@ -16,13 +17,10 @@ public class MyRemoteService extends Service {
     }
 
     MyAidlService.Stub mBinder = new MyAidlService.Stub() {
-        @Override
-        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
-
-        }
 
         @Override
         public int plus(int a, int b) throws RemoteException {
+            Log.d("TAG","a: " + a + "\t b: " + b );
             return a + b;
         }
 
